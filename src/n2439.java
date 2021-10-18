9ada21629d695de87726fe7ce90a7506fd95a2e6
YSH <94skyoun@naver.com>
2021-10-18 18:22:53 +0900

    ing...

---
+++ src/n2439.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class n2439 {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+
+    String[] k = new String[n];
+
+    for(int i=n-1; i>=0; i--){
+      System.out.println(k);
+      System.out.println("ing....");
+    }
+
+  }
+}
